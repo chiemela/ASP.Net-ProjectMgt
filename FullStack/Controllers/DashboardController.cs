e9779e2e5cc2da60a2567a5752dec5e3fde06366
Chiemela <Chiemela242@gmail.com>
2023-04-17 14:48:36 +0100

    File upload interface completed

---
+++ FullStack/Controllers/DashboardController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FullStack.Controllers
+{
+    public class DashboardController : Controller
+    {
+        // this sets access levels
+        [Microsoft.AspNetCore.Authorization.Authorize(Roles = "Admin,Lecturer,Student")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ FullStack/Data/ApplicationDBContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using FullStack.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+
+namespace FullStack.Data
+{
+    public class ApplicationDBContext : IdentityDbContext
+    {
+        public ApplicationDBContext(DbContextOptions options) : base(options)
+        {
+            
+        }
+
+        public DbSet<SalesLeadEntity> SalesLead { get; set; }
+        public DbSet<ApplicationUser> ApplicationUser { get; set; }
+        public DbSet<Module> Module { get; set; }
+        public DbSet<Enrollment> Enrollment { get; set; }
+        public DbSet<Assessment> Assessment { get; set; }
+        public DbSet<Feedback> Feedback { get; set; }
+        public DbSet<Notifications> Notifications { get; set; }
+        public DbSet<Tasks> Tasks { get; set; }
+        public DbSet<UserModules> UserModules { get; set; }
+        public DbSet<AttachmentFile> AttachmentFile { get; set; }
+    }
+}
