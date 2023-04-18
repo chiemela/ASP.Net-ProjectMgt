a93ced83eba33701d75d8227b69942a5848a7ae3
Chiemela <Chiemela242@gmail.com>
2023-04-18 21:11:23 +0100

    dashboard almost completed

--- FullStack/Controllers/DashboardController.cs
+++ FullStack/Controllers/DashboardController.cs
@@ -1,14 +1,36 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using FullStack.Data;
+using FullStack.Models;
+using FullStack.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Diagnostics;
+using static NuGet.Packaging.PackagingConstants;
 
 namespace FullStack.Controllers
 {
     public class DashboardController : Controller
     {
+        private readonly ApplicationDBContext _context;
+
+        public DashboardController(ApplicationDBContext context)
+        {
+            _context = context;
+        }
+
         // this sets access levels
-        [Microsoft.AspNetCore.Authorization.Authorize(Roles = "Admin,Lecturer,Student")]
+        // [Microsoft.AspNetCore.Authorization.Authorize(Roles = "Admin,Lecturer,Student")]
         public IActionResult Index()
         {
-            return View();
+            var NewFeedbacks = _context.NewFeedbacks.ToList();
+            var sum = NewFeedbacks.Sum(x => x.Grade);
+
+            DashboardViewModel vm = new DashboardViewModel
+            {
+                FeedbacksVM = NewFeedbacks,
+                Sum = sum
+            };
+
+            return View(vm);
         }
     }
 }
---
+++ FullStack/ViewModels/DashboardViewModel.cs
@@ -0,0 +1,12 @@
+﻿using FullStack.Models;
+
+namespace FullStack.ViewModels
+{
+    public class DashboardViewModel
+    {
+        public List<Feedbacks>? FeedbacksVM { get; set; }
+        //public List<Table2> Table2Data { get; set; }
+        public int Sum { get; set; }
+
+    }
+}
